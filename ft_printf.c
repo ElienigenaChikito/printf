761601636c2e837243ba80be2729b2821602a95a
ElienigenaChikito <eliasnoutilizado@gmail.com>
2025-04-03 14:35:19 +0200

    empiezo

---
+++ ft_printf.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <stdarg.h>
+
+int ft_putchar;
+int ft_putstr;
+int ft_putnbr;
+
+int ft_printf(const char *frase, ...)
+{
+
+}
+
+
+
+void    main()
+{
+    ft_putchar("ahhhh include");
+}
